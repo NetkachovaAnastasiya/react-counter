7ba2c5fa9548b91781b9ccf730f8124b46b88a63
Nastya <netkachova.nastya@gmail.com>
2021-05-13 16:04:21 +0300

    modofied files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    count: 0
+  }
+
+  increment = () => {
+    this.setState({ count: this.state.count + 1 })
+  }
+
+  decrement = () => {
+    this.setState({ count: this.state.count - 1 })
+  }
+
+  reset = () => {
+    this.setState({ count: 0 })
+  }
+
+  render() {
+    return (
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <div>
+          <p style={{ textAlign: 'center', fontSize: 40 }}>{this.state.count}</p>
+          <button onClick={this.increment} style={{ fontSize: 20, marginRight: 10 }}>Increment</button>
+          <button onClick={this.decrement}  style={{ fontSize: 20, marginRight: 10 }}>Decrement</button>
+          <button onClick={this.reset}  style={{ fontSize: 20, marginRight: 10 }}>Reset</button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
